7792d710c327ec14e9b81dfc05f392f91752d386
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2023-11-05 12:19:05 +0530

    Final Commit

--- core/settings.py
+++ core/settings.py
@@ -84,12 +84,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "recipedb",
+        "USER": "virendra",
+        "PASSWORD": "9021141202",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
